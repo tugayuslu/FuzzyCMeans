a787a36684a5c71abd6ca2a8749f1821c6b2e103
tugayuslu <45495524+tugayuslu@users.noreply.github.com>
2019-03-16 21:47:32 +0300

    Add files via upload

---
+++ FuzzyCMeans/FuzzyCMeans/YeniDeneme/YeniDeneme/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace FuzzyCMeans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.kumeSayisiTB = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.parametreTB = new System.Windows.Forms.TextBox();
+            this.epsilonTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbRandom = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(178, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 45);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(261, 400);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(9, 10);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(58, 26);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sheets";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(275, 45);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(638, 400);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // kumeSayisiTB
+            // 
+            this.kumeSayisiTB.Location = new System.Drawing.Point(375, 15);
+            this.kumeSayisiTB.Name = "kumeSayisiTB";
+            this.kumeSayisiTB.Size = new System.Drawing.Size(60, 20);
+            this.kumeSayisiTB.TabIndex = 4;
+            this.kumeSayisiTB.Text = "2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(852, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 26);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Heap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cluster Count";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(464, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Parameter";
+            // 
+            // parametreTB
+            // 
+            this.parametreTB.Location = new System.Drawing.Point(524, 15);
+            this.parametreTB.Name = "parametreTB";
+            this.parametreTB.Size = new System.Drawing.Size(60, 20);
+            this.parametreTB.TabIndex = 4;
+            this.parametreTB.Text = "2";
+            // 
+            // epsilonTB
+            // 
+            this.epsilonTB.Location = new System.Drawing.Point(660, 15);
+            this.epsilonTB.Name = "epsilonTB";
+            this.epsilonTB.Size = new System.Drawing.Size(60, 20);
+            this.epsilonTB.TabIndex = 8;
+            this.epsilonTB.Text = "0,01";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(615, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Epsilon";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(919, 45);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 400);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(972, 20);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "List of J Values";
+            // 
+            // cbRandom
+            // 
+            this.cbRandom.AutoSize = true;
+            this.cbRandom.Location = new System.Drawing.Point(749, 16);
+            this.cbRandom.Name = "cbRandom";
+            this.cbRandom.Size = new System.Drawing.Size(66, 17);
+            this.cbRandom.TabIndex = 11;
+            this.cbRandom.Text = "Random";
+            this.cbRandom.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 451);
+            this.Controls.Add(this.cbRandom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.epsilonTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.parametreTB);
+            this.Controls.Add(this.kumeSayisiTB);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fuzzy C Means";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox kumeSayisiTB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox parametreTB;
+        private System.Windows.Forms.TextBox epsilonTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cbRandom;
+    }
+}
+
---
+++ FuzzyCMeans/FuzzyCMeans/YeniDeneme/YeniDeneme/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using ExcelDataReader;
+
+namespace FuzzyCMeans
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet result;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xls", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    IExcelDataReader reader = ExcelReaderFactory.CreateBinaryReader(fs);
+
+                    result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+                    comboBox1.Items.Clear();
+                    foreach (DataTable dt in result.Tables)
+                    {
+                        comboBox1.Items.Add(dt.TableName);
+                    }
+                    reader.Close();
+                    comboBox1.SelectedIndex = 0;
+                    cbRandom.Checked = true;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = result.Tables[comboBox1.SelectedIndex];
+            Point_Ciz(comboBox1.SelectedIndex);
+
+        }
+
+        private void Point_Ciz(int index)
+        {
+            chart1.Series.Clear();
+            chart1.Series.Add("Veriler");
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                chart1.Series["Veriler"].Points.Add(new DataPoint(Convert.ToDouble(dataGridView1[0, i].Value.ToString()), Convert.ToDouble(dataGridView1[1, i].Value.ToString())));
+            }
+            chart1.Series["Veriler"].ChartType = SeriesChartType.Point;
+            //chart1.Series["Veriler"].MarkerStyle = MarkerStyle.Square;
+            //chart1.Series["Veriler"].MarkerSize = 15;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            int itesayar = 1;
+            bool islemsizFlag = false;
+            int kumeSayisi = Convert.ToInt32(kumeSayisiTB.Text);
+            double parametre = Convert.ToDouble(parametreTB.Text);
+            double epsilon = Convert.ToDouble(epsilonTB.Text);
+
+            if (kumeSayisi > dataGridView1.RowCount)
+            {
+                kumeSayisi = dataGridView1.RowCount;
+                kumeSayisiTB.Text = kumeSayisi.ToString();
+            }
+
+            if (!(parametre > 1))
+            {
+                parametre = 2;
+                parametreTB.Text = parametre.ToString();
+                islemsizFlag = true;
+                goto islemsiz;
+            }
+
+            if (parametre > 30)
+            {
+                parametre = 30;
+                parametreTB.Text = parametre.ToString();
+                islemsizFlag = true;
+                goto islemsiz;
+            }
+
+
+            List<double> X = new List<double>();
+            List<double> Y = new List<double>();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                X.Add(Convert.ToDouble(dataGridView1[0, i].Value));
+                Y.Add(Convert.ToDouble(dataGridView1[1, i].Value));
+            }
+            double[,] Uyelik = new double[kumeSayisi, dataGridView1.RowCount];
+            double[,] distance = new double[kumeSayisi, dataGridView1.RowCount];
+            double[,] Merkezler = new double[kumeSayisi, 2];
+
+            if(cbRandom.Checked)
+            {
+                //random
+                Random rnd = new Random();
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < kumeSayisi; j++)
+                    {
+                        Uyelik[j, i] = 0;
+                    }
+                    Uyelik[rnd.Next(kumeSayisi),i] = 1;
+                }
+            }
+            else
+            {
+                //manuel
+                for (int i = 0; i < kumeSayisi; i++)
+                {
+                    for (int j = 0; j < dataGridView1.RowCount; j++)
+                    {
+                        Uyelik[i, j] = 0;
+                    }
+                    Uyelik[i, i] = 1;
+                }
+                for (int i = kumeSayisi; i < dataGridView1.RowCount; i++)
+                {
+                    Uyelik[kumeSayisi - 1, i] = 1;
+                }
+            }
+
+            
+            dondur:
+            double nuToplam = 0, temp = 0, toplamX = 0, toplamY = 0;
+            //kume merkezleri
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                nuToplam = 0;
+                toplamX = 0;
+                toplamY = 0;
+                for (int k = 0; k < dataGridView1.RowCount; k++)
+                {
+                    temp = Math.Pow(Uyelik[i, k], parametre);
+                    toplamX += temp * X[k];
+                    toplamY += temp * Y[k];
+                    nuToplam += temp;
+                }
+                Merkezler[i, 0] = toplamX / nuToplam;
+                Merkezler[i, 1] = toplamY / nuToplam;
+            }
+
+            //distance matrisi küme merkezine uzaklıklar
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    distance[i, j] = Math.Sqrt(Math.Pow((Merkezler[i, 0] - X[j]), 2) + Math.Pow((Merkezler[i, 1] - Y[j]), 2));
+                }
+            }
+            
+
+            //ayırma matrisi
+            double kulToplam = 0;
+            bool flag = true;
+            double[,] Uyelik2 = new double[kumeSayisi, dataGridView1.RowCount];
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    kulToplam = 0; flag = true;
+                    for (int k = 0; k < kumeSayisi; k++)
+                    {
+                        if (distance[i, j] != 0 && distance[k, j] != 0)
+                        {
+                            kulToplam += Math.Pow((distance[i, j] / distance[k, j]), (2 / (parametre - 1)));
+                        }
+                        else if (distance[i, j] == 0 && distance[k, j] == 0)
+                        {
+                            kulToplam += 1;
+                        }
+                        else if (distance[i, j] != 0 && distance[k, j] == 0)
+                        {
+                            kulToplam = 0;
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        kulToplam = 1 / kulToplam;
+                    }
+
+                    Uyelik2[i, j] = kulToplam;
+                }
+            }
+
+            //epsilon kontrol
+            bool bayrak = true;
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                if (!bayrak)
+                    break;
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    if (epsilon < Math.Abs(Uyelik[i, j] - Uyelik2[i, j]))
+                    {
+                        bayrak = false;
+                        break;
+                    }
+                }
+            }
+
+            Uyelik = Uyelik2;
+
+            //iterasyon değeri kısmı
+            double jdegeri = 0;
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    jdegeri += Math.Pow(Uyelik[i, j], parametre) * Math.Pow(distance[i, j], 2);
+                }
+            }
+
+            richTextBox1.Text += itesayar + ". İterasyon J = " + jdegeri + "\n";
+            itesayar++;
+
+
+            //donuyor mu kontrol
+            if (!bayrak)
+                goto dondur;
+
+
+
+            chart1.Series.Clear();
+
+            List<string> kumeIsımler = new List<string>();
+            kumeIsımler.Add("Merkezler");
+            for (int i = 1; i < kumeSayisi + 1; i++)
+            {
+                kumeIsımler.Add("Kume " + i);
+            }
+            foreach (var item in kumeIsımler)
+            {
+                chart1.Series.Add(item);
+
+            }
+            chart1.Series[kumeIsımler[0]].MarkerStyle = MarkerStyle.Square;
+            chart1.Series[kumeIsımler[0]].MarkerSize = 15;
+
+
+            //kume kararı
+            double buyukKume = 0;
+            int indis = 0;
+            int indis2 = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                buyukKume = 0; indis = 0; indis2 = 0;
+                for (int j = 0; j < kumeSayisi; j++)
+                {
+                    if (Uyelik[j, i] > buyukKume)
+                    {
+                        buyukKume = Uyelik[j, i];
+                        indis = j;
+                        indis2 = i;
+                    }
+                }
+                chart1.Series[indis + 1].Points.Add(new DataPoint(X[indis2], Y[indis2]));
+            }
+
+            //merkezler eklenir
+            for (int i = 0; i < kumeSayisi; i++)
+            {
+                chart1.Series[kumeIsımler[0]].Points.Add(new DataPoint(Merkezler[i, 0], Merkezler[i, 1]));
+            }
+
+            //grafik şekli seçiliyor
+            for (int i = 1; i < kumeIsımler.Count(); i++)
+            {
+                chart1.Series[kumeIsımler[i]].ChartType = SeriesChartType.Point;
+            }
+            chart1.Series[kumeIsımler[0]].ChartType = SeriesChartType.Point;
+            
+
+
+            islemsiz:
+            if(islemsizFlag)
+            {
+                MessageBox.Show("m' Parametre 1'den büyük,30'dan küçük olmalı");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
